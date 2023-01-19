ccadb4f76f93f241fce834977b647acc768c3abd
ParkJoungWoo <jug78@naver.com>
2023-01-20 03:55:24 +0900

    ignore재지정

--- cotato/src/main/java/com/cotato/book/springboot/domain/posts/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cotato.book.springboot.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long>{
-
-}
