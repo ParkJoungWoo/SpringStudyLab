fffba67635fdacb879cd4d19308d0c7c4fb6db8e
ParkJoungWoo <jug78@naver.com>
2023-01-20 03:52:52 +0900

    3장 java파일

--- cotato/src/main/java/com/cotato/book/springboot/service/posts/PostsService.java
+++ cotato/src/main/java/com/cotato/book/springboot/service/posts/PostsService.java
@@ -1,2 +1,36 @@
-package com.cotato.book.springboot.service.posts;public class PostsService {
+package com.cotato.book.springboot.service.posts;
+
+import com.cotato.book.springboot.domain.posts.Posts;
+import com.cotato.book.springboot.domain.posts.PostsRepository;
+import com.cotato.book.springboot.web.dto.PostsResponseDto;
+import com.cotato.book.springboot.web.dto.PostsSaveRequestDto;
+import com.cotato.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).
+                                                    getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id:" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id:" + id));
+        return new PostsResponseDto(entity);
+    }
 }
--- cotato/src/main/java/com/cotato/book/springboot/web/PostsApiController.java
+++ cotato/src/main/java/com/cotato/book/springboot/web/PostsApiController.java
@@ -1,2 +1,29 @@
-package com.cotato.book.springboot.web;public class PostsApiController {
+package com.cotato.book.springboot.web;
+
+import com.cotato.book.springboot.service.posts.PostsService;
+import com.cotato.book.springboot.web.dto.PostsResponseDto;
+import com.cotato.book.springboot.web.dto.PostsSaveRequestDto;
+import com.cotato.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
